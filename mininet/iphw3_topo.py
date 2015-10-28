1edd48d739b46109685e99b1ae38779e5adae6e6
sinampu <sinampu@ncsu.edu>
2015-10-28 16:21:23 -0400

    changed topos

--- mininet/iphw3_topo.py
+++ mininet/iphw3_topo.py
@@ -2,17 +2,15 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 
 net = Mininet()
-
-c0 = net.addController()
 h0 = net.addHost('h0')
-s0 = net.addSwitch('s0')
 h1 = net.addHost('h1')
+h2 = net.addHost('h2')
+h3 = net.addHost('h3')
 
-net.addLink(h0,s0)
-net.addLink(h1,s0)
+net.addLink(h0,h1)
+net.addLink(h1,h2)
+net.addLink(h2,h3)
 
-h0.setIP('10.0.0.0',24)
-h1.setIP('10.0.0.1',24)
 
 net.start()
 net.pingAll()
